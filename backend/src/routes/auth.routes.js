83acee8fba6430fa6bb23c255d08fb247e4258d2
Mario García <mariogrdg02@outlook.com>
2025-11-18 10:46:23 +0100

    refactor: signOut es ruta protegida

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { signIn, signOut, signUp } from "../controllers/auth.controller.js"
+import protectRoute from "../middleware/protectRoute.js"
+
 
 const router = express.Router()
 
 router
     .post("/", signUp)
     .post("/signin", signIn)
-    .post("/signout", signOut)
+    .post("/signout", protectRoute, signOut)
 
 export default router
