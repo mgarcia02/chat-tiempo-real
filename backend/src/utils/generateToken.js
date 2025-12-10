cfdd609e887efe60783138cc7ce01199ac38093f
Mario García <mariogrdg02@outlook.com>
2025-12-10 10:43:58 +0100

    fix: cambia duracion del token

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -5,10 +5,10 @@ dotenv.config()
 const jwtSecret = process.env.JWT_SECRET;
 
 export const generateTokenAndSetCookie = (userId, res) => {
-    const token = jwt.sign({userId}, jwtSecret, {expiresIn: "1d"})
+    const token = jwt.sign({userId}, jwtSecret, {expiresIn: "8h"})
 
     res.cookie("token", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000,
+        maxAge: 8 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
         secure: process.env.NODE_ENV !== "development"
